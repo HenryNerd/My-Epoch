e0e018668f35fde7aa4f9e912804116bd5498435
Henry Veedahl <henry.veedahl@outlook.com>
2025-04-11 21:35:46 -0500

    Synchroise Repositorys

--- My Epoch/ContentView.swift
+++ My Epoch/ContentView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var userInput = ""
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
+                .font(.system(size: 52))
             Text("Hello, world!")
+                .font(.system(size: 52))
         }
         .padding()
+        TextField("What are you working on?", text: $userInput)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                        .font(.system(size: 22))
+        Button("Enter") {
+                        UserDefaults.standard.set(userInput, forKey: "savedText")
+                    }
+        .frame(width: 366, height: 45)
+        .background(Color("AccentColor"))
+        .foregroundColor(.white)
+        .font(.system(size: 31))
+        .cornerRadius(20)
+        
     }
 }
 
