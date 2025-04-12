a3b93082bbba6caf029a4afd7dbc08b24338d843
Henry Veedahl <henry.veedahl@outlook.com>
2025-04-11 20:30:35 -0500

    Initial Commit

---
+++ My Epoch/My_EpochApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_EpochApp.swift
+//  My Epoch
+//
+//  Created by Henry Veedahl on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct My_EpochApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ My EpochTests/My_EpochTests.swift
@@ -0,0 +1,17 @@
+//
+//  My_EpochTests.swift
+//  My EpochTests
+//
+//  Created by Henry Veedahl on 4/11/25.
+//
+
+import Testing
+@testable import My_Epoch
+
+struct My_EpochTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
