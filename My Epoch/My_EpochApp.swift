7c3b415de8a14824cade803bb023f6de441601e8
Henry Veedahl <henry.veedahl@outlook.com>
2025-04-12 20:43:23 -0500

    Fix Bugs

---
+++ My Epoch/My_EpochApp.swift
@@ -0,0 +1,23 @@
+//
+//  My_EpochApp.swift
+//  My Epoch
+//
+//  Created by Henry Veedahl on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct My_EpochApp: App {
+    @AppStorage("userInputText") private var savedText: String = ""
+    
+    var body: some Scene {
+        WindowGroup {
+            Text("Saved text: \(savedText)")
+                .font(.title)
+                .padding()
+            ContentView()
+                .navigationTitle("StartDate")
+        }
+    }
+}
--- My Epoch/StartDate.swift
+++ My Epoch/StartDate.swift
@@ -1,33 +1,15 @@
+//
+//  StartDate.swift
+//  My Epoch
+//
+//  Created by Henry Veedahl on 4/11/25.
+//
+
 import SwiftUI
 
 struct StartDate: View {
-    @AppStorage("userInputText") private var savedText: String = ""
-    @State private var selectedDate = Date()
-
     var body: some View {
-        VStack {
-            Text("Saved text: \(savedText)")
-                .font(.title)
-                .padding()
-
-            DatePicker("Select a Date", selection: $selectedDate, displayedComponents: .date)
-                .datePickerStyle(GraphicalDatePickerStyle())
-                .padding()
-
-            Text("You selected: \(selectedDate, formatter: DateFormatter.shortDate)")
-                .font(.title2)
-                .padding()
-        }
-        .padding()
-        .navigationTitle("Start Date");
-    }
-}
-
-extension DateFormatter {
-    static var shortDate: DateFormatter {
-        let formatter = DateFormatter()
-        formatter.dateStyle = .short
-        return formatter
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
